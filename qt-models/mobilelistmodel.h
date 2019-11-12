4cd90e65d3b01b52c5df51d5264ded5b47dcba94
Dirk Hohndel <dirk@hohndel.org>
2019-11-11 21:34:22 -0800

    Mobile: trip date and title roles for new model
    Also add the function to show how many dives are included with the current filter.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- qt-models/mobilelistmodel.h
+++ qt-models/mobilelistmodel.h
@@ -12,6 +12,8 @@ public:
 		DiveDateRole,
 		TripIdRole,
 		TripNrDivesRole,
+		TripShortDateRole,
+		TripTitleRole,
 		DateTimeRole,
 		IdRole,
 		NumberRole,
@@ -49,6 +51,7 @@ public:
 	void expand(const QModelIndex &index);
 	void unexpand();
 	void flip(const QModelIndex &index);
+	Q_INVOKABLE int shown(); // number dives that are accepted by the filter
 private:
 	void connectSignals();
 	int numSubItems() const;
