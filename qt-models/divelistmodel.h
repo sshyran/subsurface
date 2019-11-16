ad6572dd041eb1b01af921428e0badbec4229242
Dirk Hohndel <dirk@hohndel.org>
2019-11-15 21:38:01 -0800

    Mobile: remove collapsed model that we no longer use
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- qt-models/divelistmodel.h
+++ qt-models/divelistmodel.h
@@ -7,36 +7,6 @@
 
 #include "core/subsurface-qt/DiveObjectHelper.h"
 
-class CollapsedDiveListSortModel : public QSortFilterProxyModel
-{
-	Q_OBJECT
-public:
-	static CollapsedDiveListSortModel *instance();
-	void setSourceModel(QAbstractItemModel *sourceModel);
-	Q_INVOKABLE QString tripTitle(const QString &trip);
-	Q_INVOKABLE QString tripShortDate(const QString &trip);
-	Q_INVOKABLE void setActiveTrip(const QString &trip);
-	Q_INVOKABLE QString activeTrip() const;
-	// super subtle optimization alert - in order to reduce the number of model accesses from QML,
-	// the two states where we show the dive in question have odd numbers
-	enum CollapsedState {
-		DontShow = 0,
-		ShowDive = 1,
-		ShowTrip = 2,
-		ShowDiveAndTrip = 3
-	};
-	void updateFilterState();
-	void updateSelectionState();
-
-protected:
-	bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const;
-
-private:
-	CollapsedDiveListSortModel();
-	bool isExpanded(struct dive_trip *dt) const;
-	QString m_activeTrip;
-};
-
 class DiveListSortModel : public QSortFilterProxyModel
 {
 	Q_OBJECT
@@ -103,7 +73,6 @@ public:
 		StartPressureRole,
 		EndPressureRole,
 		FirstGasRole,
-		CollapsedRole,
 		SelectedRole,
 	};
 
